5aaa7e6d3f3db2a0609ee7c68b04e9613a4da6d7
Mallne <info@mallne.cloud>
2026-09-21 19:10:13 +0200

    build: add units build inclusion for monorepo context detection
    - Includes `../units` as a build dependency when present.
    - Prints a warning when running outside the monorepo context.
    
    Signed-off-by: Mallne <info@mallne.cloud>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,18 @@ pluginManagement {
     }
 }
 
+val unitsDir = file("../units")
+if (unitsDir.exists()) {
+    includeBuild(unitsDir.absolutePath) {
+        dependencySubstitution {
+            substitute(module("cloud.mallne:units")).using(project(":"))
+        }
+    }
+} else {
+    println("[GEOKIT:units] This Project seems to be running without the Monorepo Context, please consider using the Monorepo")
+}
+
+
 include(":calculation")
 include(":geojson")
 include(":ogc")
